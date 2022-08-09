1d4e0a10c047f88f3278a9a1949bb04ae69f6eb6
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2022-08-09 11:50:42 -0600

    Cambios en prints

--- DPLLV2.py
+++ DPLLV2.py
@@ -20,14 +20,10 @@ def DPLL(cnf, assignments={}):
 		if(literal,True) not in clausula:
 			nuevo_cnf.append(clausula)
 
-	# print(nuevo_cnf)
-
 	nuevo_cnf_2 = []
 
 	for clausula in nuevo_cnf:
 		nuevo_cnf_2.append(clausula.difference({(literal,False)}))
-
-	# print(nuevo_cnf_2)
 
 	sat, valores = DPLL(nuevo_cnf_2, {**assignments, **{literal: True}})
 
@@ -42,11 +38,16 @@ def DPLL(cnf, assignments={}):
 
 	nuevo_cnf_2 = []
 
+	print('Clausula 1: ',{**assignments, **{literal: True}})
+
 	for clausula in nuevo_cnf:
 		nuevo_cnf_2.append(clausula.difference({(literal,True)}))
 
+	print('Clausula 2: ',{**assignments, **{literal: False}})
+
 	sat, valores = DPLL(nuevo_cnf_2, {**assignments, **{literal: False}})
 	
+
 	if sat:
 		return sat, valores
 
@@ -54,5 +55,14 @@ def DPLL(cnf, assignments={}):
 
 
 cnf = [{("f", False), ("p", False), ("q", False)}] #Se define la clausula.
-
-print(DPLL(cnf)) #Se imprime el resultado.
+print(DPLL(cnf))
+cnf = [{('a', False), ('b', False)}, {('a', False), ('b', False)}]
+print(DPLL(cnf))
+cnf = [{("p", False), ("q", True)}, {("p", False), ("r", False)}]
+print(DPLL(cnf))
+cnf = [{("p", False), ("q", True)}, {("p", False), ("r", False)}, {("q", False), ("r", False)}]
+print(DPLL(cnf))
+cnf = [{("p", False), ("q", True)}, {("p", False), ("r", False)}, {("q", False), ("r", False)}, {("p", True), ("q", False), ("r", False)}]
+print(DPLL(cnf))
+cnf = [{("p", True)}, {("p", False)}]
+print(DPLL(cnf))
