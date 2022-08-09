d450617ce2c3f28093dd5d51af4f307e72b69a5d
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2022-08-09 12:03:52 -0600

    Agregados comentarios a DPLL

--- DPLLV2.py
+++ DPLLV2.py
@@ -1,35 +1,43 @@
+#Metodo utulizado para obtener el literal a evaluar
 def obtenerLiteral(cnf):
 	for clausula in cnf:
 		for caracter in clausula:
 			return caracter[0]
 
 def DPLL(cnf, assignments={}):
-	
+	# Si el length de la clausula es 0 se retorna True junto con las asignaciones resultantes.
 	if not cnf:
 		return True, assignments
 	
+	# Si el size de alguna clausula es 0 se retorna False.
 	for clausula in cnf:
 		if len(clausula) == 0:
 			return False, None
 	
+	# Se obtiene el primer literal que se encuentre en la clausula.
 	literal = obtenerLiteral(cnf)
 
+	# Se eliminan todas las clausulas que contengan el literal l en el cnf.
 	nuevo_cnf = []
 
 	for clausula in cnf:
 		if(literal,True) not in clausula:
 			nuevo_cnf.append(clausula)
 
+	# Se eliminan las ocurriencias del literal l en las clausulas para el cnf y obtener el nuevo cnf' == cnf_2.
 	nuevo_cnf_2 = []
 
 	for clausula in nuevo_cnf:
 		nuevo_cnf_2.append(clausula.difference({(literal,False)}))
 
+	# Al resultado de cnf' se le aplica el algoritmo con una I' la cual se compone de I U l:True.
 	sat, valores = DPLL(nuevo_cnf_2, {**assignments, **{literal: True}})
 
+	# Si el resultado de SAT es True se retornan los valores.
 	if sat:
 		return sat, valores
 
+	# En el caso de no acceder al previo If, se realiza la misma prueba en el complemento del conjunto:
 	nuevo_cnf = []
 
 	for clausula in cnf:
@@ -47,10 +55,11 @@ def DPLL(cnf, assignments={}):
 
 	sat, valores = DPLL(nuevo_cnf_2, {**assignments, **{literal: False}})
 	
-
+	# Si el complemento del conjunto es verdadero se retorna True junto a sus valores.
 	if sat:
 		return sat, valores
 
+	# Si no se retorna False.
 	return False, None
 
 
